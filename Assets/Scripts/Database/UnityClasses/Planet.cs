947ef153ee80730f410ecf7f57faf4b8fa62994c
Oğuz Kerem Tural <o.kerem.tural@gmail.com>
2016-02-04 10:05:02 +0200

    Added missing planet files.

---
+++ Assets/Scripts/Database/UnityClasses/Planet.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Divan;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+/// <summary>
+/// Planet class, unity compatible version of PlanetInfo class. 
+/// It contains planet information gathered from db.
+/// </summary>
+public class Planet : MonoBehaviour
+{
+    // Planet information.
+    public PlanetInfo planetInfo;
+
+    /// <summary>
+    /// Default consructor. Only creates an empty instance of the object.
+    /// </summary>
+    public Planet() { }
+
+    /// <summary>
+    /// After object has been created, it should have initialized
+    /// through this method.
+    /// </summary>
+    /// <param name="planetToken"></param>
+    public void GetPlanet(JToken planetToken)
+    {
+        planetInfo = new PlanetInfo(planetToken);
+    }
+}
+
